a5ba4b56b6639d018578ef25bc6a61c11be37de1
Kirill Rasputin <rasputinkirill@gmail.com>
2020-02-27 15:33:48 +0700

    UGENE-6649 Invalid extracted PCR product when a primer goes through the circular sequence start coordinate

--- src/plugins/pcr/src/InSilicoPcrTask.cpp
+++ src/plugins/pcr/src/InSilicoPcrTask.cpp
@@ -161,8 +161,10 @@ InSilicoPcrTask::PrimerBind InSilicoPcrTask::getPrimerBind(const FindAlgorithmRe
         const qint64 reverseRegionEndPos = reverse.region.endPos();
         const qint64 sequenceSize = settings.sequence.size();
         if (reverseRegionEndPos > sequenceSize) {
-            result.region = U2Region(reverse.region.startPos, sequenceSize - reverse.region.startPos);
-            if (!settings.isCircular) {
+            if (settings.isCircular) {
+                result.region = U2Region(reverse.region.startPos, qAbs(sequenceSize - reverse.region.startPos));
+            } else {
+                result.region = U2Region(reverse.region.startPos, sequenceSize);
                 result.region.length -= reverse.region.startPos;
             }
             result.ledge = reverseRegionEndPos - sequenceSize;
