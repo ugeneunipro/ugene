492e64c708e3c543eac2f934851b80b897762ae6
Kirill Rasputin <rasputinkirill@gmail.com>
2020-02-27 15:52:06 +0700

    UGENE-6649

--- src/plugins/pcr/src/InSilicoPcrTask.cpp
+++ src/plugins/pcr/src/InSilicoPcrTask.cpp
@@ -162,7 +162,7 @@ InSilicoPcrTask::PrimerBind InSilicoPcrTask::getPrimerBind(const FindAlgorithmRe
         const qint64 sequenceSize = settings.sequence.size();
         if (reverseRegionEndPos > sequenceSize) {
             if (settings.isCircular) {
-                result.region = U2Region(reverse.region.startPos, qAbs(sequenceSize - reverse.region.startPos));
+                result.region = U2Region(reverse.region.startPos, sequenceSize - reverse.region.startPos);
             } else {
                 result.region = U2Region(reverse.region.startPos, sequenceSize);
                 result.region.length -= reverse.region.startPos;
