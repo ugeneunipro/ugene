3ed75b3d6d1691e9a4d36710ea70a1e20a9611b1
DmitriiSukhomlinov <sukhomlinovdmitrii@yandex.ru>
2021-01-13 17:04:01 +0700

    UGENE-6948 replace NIAID with Cistrome

--- src/corelibs/U2Test/src/gui_tests/UGUITest.h
+++ src/corelibs/U2Test/src/gui_tests/UGUITest.h
@@ -50,8 +50,8 @@ public:
     /** A label to mark test as included into the 'Metagenomics' teamcity build suite. */
     static const QString Metagenomics;
 
-    /** A label to mark test as included into the 'NIAID' teamcity build suite. */
-    static const QString Niaid;
+    /** A label to mark test as included into the 'Cistrome' teamcity build suite. */
+    static const QString Cistrome;
 
     /** A test with this label is safe to run on Linux. */
     static const QString Linux;
