b0aa9bee0acec4121162d162df7b009b571a91ca
Igor Chebykin <ichebyki@ugene-macbook-2.unipro.ru>
2021-04-25 02:08:34 +0700

    Add notarization scripts for macos

--- installer/macosx/productbuild.sh
+++ installer/macosx/productbuild.sh
@@ -3,8 +3,18 @@ set -x
 
 if [[ -f `dirname $0`/utils.sh ]]; then source `dirname $0`/utils.sh; fi
 
-if [[ -z "$1" ]]; then echo ERROR!; exit -1; fi
-if [[ ! -d "$1" ]]; then echo ERROR!; exit -1; fi
+if [[ -z "$1" ]]; then 
+    echo "ERROR: 1st arg must be application dir!" 
+    exit -1
+fi
+if [[ -z "$2" ]]; then 
+    echo "ERROR: 2nd arg must be package name!" 
+    exit -2
+fi
+if [[ -z "$3" ]]; then 
+    echo "ERROR: 3rd arg must be new signed package name!" 
+    exit -2
+fi
 
 productbuild \
     --component "$1" \
