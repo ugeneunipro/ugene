cea1814f4cf14c8c81fbbcbf8b30a9f5427affd4
Yuliya Algaer <yalgaer@gmail.com>
2021-05-03 21:01:59 -0400

    Fix hardcoded path in tests + tune up ideal test counts

--- src/plugins/GUITestBase/src/harness/GUITestLauncher.cpp
+++ src/plugins/GUITestBase/src/harness/GUITestLauncher.cpp
@@ -143,7 +143,7 @@ QList<GUITest *> getIdealNightlyTestsSplit(int suiteIndex, int suiteCount, const
     } else if (suiteCount == 4) {
         testsPerSuite << 640 << 680 << 640 << -1;
     } else if (suiteCount == 5) {
-        testsPerSuite << 540 << 575 << 465 << 535 << -1;
+        testsPerSuite << 545 << 575 << 470 << 540 << -1;
     }
     QList<GUITest *> tests;
     if (testsPerSuite.size() == suiteCount) {
