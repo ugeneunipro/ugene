07af675ce3b92e90997ec694ffaa2c1a78f95863
Evelina Biserova <biserova@unipro.ru>
2021-06-23 12:47:35 +0700

    UGENE-7247. Application unexpectedly closed after saving Workflow properties. Added GUI test.

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -1744,6 +1744,7 @@ void GUITestBasePlugin::registerTests(UGUITestBase *guiTestBase) {
 
     REGISTER_TEST(GUITest_regression_scenarios::test_7212);
     REGISTER_TEST(GUITest_regression_scenarios::test_7246);
+    REGISTER_TEST(GUITest_regression_scenarios::test_7247);
     REGISTER_TEST(GUITest_regression_scenarios::test_7293);
 
     //////////////////////////////////////////////////////////////////////////
