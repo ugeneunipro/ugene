7b02afb457d2f777da11a01a91c06dc48acd0735
Yuliya Algaer <yalgaer@gmail.com>
2021-08-07 23:47:07 +0700

    UGENE-7371. SAFE_POINT if move vertical scroll bar in Sanger editor (#504)

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -1777,6 +1777,7 @@ void GUITestBasePlugin::registerTests(UGUITestBase *guiTestBase) {
 
     REGISTER_TEST(GUITest_regression_scenarios::test_7367);
     REGISTER_TEST(GUITest_regression_scenarios::test_7368);
+    REGISTER_TEST(GUITest_regression_scenarios::test_7371);
 
     //////////////////////////////////////////////////////////////////////////
     // Common scenarios/project/
