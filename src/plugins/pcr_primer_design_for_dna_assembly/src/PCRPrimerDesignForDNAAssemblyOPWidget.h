421016bc072d1e4cd7d4c011c3f270d4ce415a57
Kirill Rasputin <rasputinkirill@gmail.com>
2021-08-19 15:14:55 +0700

    Update PCRPrimerDesignForDNAAssemblyOPWidget.h
    wrong comment removed

--- src/plugins/pcr_primer_design_for_dna_assembly/src/PCRPrimerDesignForDNAAssemblyOPWidget.h
+++ src/plugins/pcr_primer_design_for_dna_assembly/src/PCRPrimerDesignForDNAAssemblyOPWidget.h
@@ -86,9 +86,6 @@ private slots:
      * Show the "Open file" dialog, choose a file with other PCR sequences and set path to the corresponding line edit.
      */
     void sl_loadOtherSequenceInPcr();
-    /*
-     * Associate created resultannotations with result table.
-     */
     void sl_onFindTaskFinished();
     /*
      * Extract result product to file.
