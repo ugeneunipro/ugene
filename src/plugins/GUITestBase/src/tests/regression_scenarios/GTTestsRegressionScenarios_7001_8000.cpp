054505dd4b4fb9478791d50f72807210e6557b75
EvelinaBiserova <39720063+EvelinaBiserova@users.noreply.github.com>
2021-09-06 21:44:58 +0700

    UGENE-7407. Single character sequences cannot be generated. (#575)
    * UGENE-7407. Single character sequences cannot be generated.
    Urgent fix.
    Added GUI test.
    
    * Using a character with the highest frequency by default
    
    Co-authored-by: Evelina Biserova <biserova@unipro.ru>
    Co-authored-by: Yuliya Algaer <yalgaer@gmail.com>

--- src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_7001_8000.cpp
+++ src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_7001_8000.cpp
@@ -833,6 +833,25 @@ GUI_TEST_CLASS_DEFINITION(test_7405) {
     CHECK_SET_ERR(error.contains(model.referenceUrl), "Expected error message is not found");
 }
 
+GUI_TEST_CLASS_DEFINITION(test_7407) {
+    // Check that UGENE can generate a single character sequence.
+    DNASequenceGeneratorDialogFillerModel model(sandBoxDir + "/test_7407.fa");
+    model.length = 1;
+    model.window = 1;
+    model.referenceUrl = testDir + "_common_data/sanger/reference.gb";
+
+    GTLogTracer lt;
+    GTUtilsDialog::waitForDialog(os, new DNASequenceGeneratorDialogFiller(os, model));
+    GTMenu::clickMainMenuItem(os, {"Tools", "Random sequence generator..."});
+
+    GTUtilsSequenceView::checkSequenceViewWindowIsActive(os);
+    QString sequence = GTUtilsSequenceView::getSequenceAsString(os);
+
+    CHECK_SET_ERR(sequence.length() == 1, "Invalid sequence length: " + QString::number(sequence.length()));
+    CHECK_SET_ERR(sequence[0] == 'T', "Invalid sequence symbol: " + sequence[0]);
+    GTUtilsLog::check(os, lt);
+}
+
 }  // namespace GUITest_regression_scenarios
 
 }  // namespace U2
