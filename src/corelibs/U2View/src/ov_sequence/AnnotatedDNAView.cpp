f8c244f26ce66ff66b78fd7bc5aabd204123f0b5
DmitriiSukhomlinov <sukhomlinovdmitrii@yandex.ru>
2021-09-10 15:54:37 +0700

    UGENE-7382 build fix

--- src/corelibs/U2View/src/ov_sequence/AnnotatedDNAView.cpp
+++ src/corelibs/U2View/src/ov_sequence/AnnotatedDNAView.cpp
@@ -262,7 +262,7 @@ QWidget *AnnotatedDNAView::createWidget() {
     QList<OPFactoryFilterVisitorInterface *> filters;
 
     ADVSequenceObjectContext *ctx;
-    QList<const DNAAlphabet *> alphabets;
+    QList<DNAAlphabetType> alphabets;
 
     for (int i = 0; i < seqViews.size(); i++) {
         if (seqViews[i] != nullptr) {
@@ -270,7 +270,7 @@ QWidget *AnnotatedDNAView::createWidget() {
             if (ctx) {
                 const DNAAlphabet *alphabet = ctx->getAlphabet();
                 if (alphabet) {
-                    alphabets.append(alphabet);
+                    alphabets.append(alphabet->getType());
                 }
             }
         }
