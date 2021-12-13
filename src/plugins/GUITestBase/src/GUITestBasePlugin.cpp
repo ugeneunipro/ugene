86e46ce9475924bf0359a751784e826220184698
Kirill Rasputin <rasputinkirill@gmail.com>
2021-12-13 23:19:40 +0700

    Test with "unload" action removed

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -1112,7 +1112,6 @@ void GUITestBasePlugin::registerTests(UGUITestBase *guiTestBase) {
 
     REGISTER_TEST(GUITest_regression_scenarios::test_3805);
     REGISTER_TEST(GUITest_regression_scenarios::test_3809);
-    REGISTER_TEST(GUITest_regression_scenarios::test_3813);
     REGISTER_TEST(GUITest_regression_scenarios::test_3815);
     REGISTER_TEST(GUITest_regression_scenarios::test_3816);
     REGISTER_TEST(GUITest_regression_scenarios::test_3817);
