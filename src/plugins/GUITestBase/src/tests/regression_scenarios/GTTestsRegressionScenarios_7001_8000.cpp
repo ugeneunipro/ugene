3f257624e714217321fe6b94fe54f0682ac24dba
Kirill Rasputin <rasputinkirill@gmail.com>
2022-05-19 00:00:57 +0700

    UGENE-7520

--- src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_7001_8000.cpp
+++ src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_7001_8000.cpp
@@ -2253,10 +2253,38 @@ GUI_TEST_CLASS_DEFINITION(test_7520) {
     const QString trimmomaticName = "Improve Reads with Trimmomatic";
     WorkflowProcessItem* trimmomaticElement = GTUtilsWorkflowDesigner::addElement(os, trimmomaticName);
 
-    TrimmomaticDialogFiller* addFiller = new TrimmomaticDialogFiller(os, steps);
+    class TrimmomaticCustomScenario : public CustomScenario {
+        void run(HI::GUITestOpStatus& os) {
+            QWidget* dialog = GTWidget::getActiveModalWidget(os);
+            auto addButton = GTWidget::findToolButton(os, "buttonAdd", dialog);
 
-    GTUtilsDialog::waitForDialog(os, addFiller);
-    TrimmomaticDialogFiller::openDialog(os, trimmomaticElement);
+            GTWidget::click(os, addButton);
+            for (int i = 0; i < 4; i++) {
+                GTKeyboardDriver::keyClick(Qt::Key_Down);
+            }
+            GTKeyboardDriver::keyClick(Qt::Key_Enter);
+            GTWidget::click(os, addButton);
+
+            GTMouseDriver::moveTo(GTWidget::getWidgetCenter(GTWidget::findWidget(os, "palindromeThreshold")));
+            QString tooltip = GTUtilsToolTip::getToolTip();
+            QString expedtedTooltip("A threshold for palindrome alignment mode. For palindromic matches, a longer alignment is possible."
+                                    " Therefore the threshold can be in the range of 30. Even though this threshold is very high" 
+                                    " (requiring a match of almost 50 bases) Trimmomatic is still able to identify very, very short adapter fragments.");
+            CHECK_SET_ERR(tooltip.contains(expedtedTooltip), QString("Actual tooltip not contains expected string. Expected string: %1").arg(expedtedTooltip));
+
+            tooltip.clear();
+            GTMouseDriver::moveTo(GTWidget::getWidgetCenter(GTWidget::findWidget(os, "palindromeLabel")));
+            tooltip = GTUtilsToolTip::getToolTip();
+            CHECK_SET_ERR(tooltip.contains(expedtedTooltip), QString("Actual tooltip not contains expected string. Expected string: %1").arg(expedtedTooltip));
+
+            GTUtilsDialog::clickButtonBox(os, dialog, QDialogButtonBox::Cancel);
+        }
+    };
+
+    GTUtilsDialog::waitForDialog(os, new TrimmomaticDialogFiller(os, new TrimmomaticCustomScenario()));
+    GTUtilsWorkflowDesigner::click(os, trimmomaticName);
+    GTUtilsWorkflowDesigner::setParameter(os, "Trimming steps", "", GTUtilsWorkflowDesigner::customDialogSelector);
+    GTUtilsTaskTreeView::waitTaskFinished(os);
 }
 
 GUI_TEST_CLASS_DEFINITION(test_7531) {
