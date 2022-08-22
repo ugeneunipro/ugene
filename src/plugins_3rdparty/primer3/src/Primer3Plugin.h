cda0b121607ffd448a93a06c58e564374beba0ca
Dmitrii Sukhomlinov <dsukhomlinov@unipro.ru>
2022-08-22 17:35:02 +0700

    UGENE-7642 add resource tracker to Primer3 task

--- src/plugins_3rdparty/primer3/src/Primer3Plugin.h
+++ src/plugins_3rdparty/primer3/src/Primer3Plugin.h
@@ -31,6 +31,12 @@
 
 namespace U2 {
 
+/*
+ * Primer3Task should lock this resource before run and unlock after finish
+ * It's required because the original "primer3" tool doesn't support parallel calculations
+ */
+#define PRIMER3_STATIC_LOCK 6
+
 class Primer3ADVContext;
 class XMLTestFactory;
 
