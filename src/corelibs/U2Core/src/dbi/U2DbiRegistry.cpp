fff1d975c8bb5bb18fb2067b4d4551134a08830d
Yuliya Algaer <yalgaer@gmail.com>
2022-10-18 16:53:19 +0700

    Tr update for U2Core

--- src/corelibs/U2Core/src/dbi/U2DbiRegistry.cpp
+++ src/corelibs/U2Core/src/dbi/U2DbiRegistry.cpp
@@ -277,7 +277,7 @@ U2DbiPool::~U2DbiPool() {
 
 U2Dbi* U2DbiPool::createDbi(const U2DbiRef& ref, bool create, U2OpStatus& os, const QHash<QString, QString>& properties) {
     U2DbiFactory* dbiFactory = AppContext::getDbiRegistry()->getDbiFactoryById(ref.dbiFactoryId);
-    CHECK_EXT(nullptr != dbiFactory, os.setError(tr("Invalid database type: %1").arg(ref.dbiFactoryId)), nullptr);
+    CHECK_EXT(nullptr != dbiFactory, os.setError(QString("Invalid database type: %1").arg(ref.dbiFactoryId)), nullptr);
     U2Dbi* result = dbiFactory->createDbi();
 
     const QString url = dbiFactory->id2Url(ref.dbiId).getURLString();
@@ -485,7 +485,7 @@ const QString DBI_ID_DELIMETER = "|";
 
 QString getDbiUrlByRef(const U2DbiRef& ref, U2OpStatus& os) {
     U2DbiFactory* dbiFactory = AppContext::getDbiRegistry()->getDbiFactoryById(ref.dbiFactoryId);
-    SAFE_POINT_EXT(nullptr != dbiFactory, os.setError(QObject::tr("Invalid database type: %1").arg(ref.dbiFactoryId)), QString());
+    SAFE_POINT_EXT(nullptr != dbiFactory, os.setError(QString("Invalid database type: %1").arg(ref.dbiFactoryId)), QString());
     return dbiFactory->id2Url(ref.dbiId).getURLString();
 }
 
@@ -493,7 +493,7 @@ QString getDbiUrlByRef(const U2DbiRef& ref, U2OpStatus& os) {
 
 QString U2DbiPool::getId(const U2DbiRef& ref, U2OpStatus& os) {
     const QString url = U2DbiUtils::ref2Url(ref);
-    SAFE_POINT_EXT(!url.isEmpty(), os.setError(tr("Invalid dbi reference")), "");
+    SAFE_POINT_EXT(!url.isEmpty(), os.setError(QString("Invalid dbi reference")), "");
     return url;
 }
 
