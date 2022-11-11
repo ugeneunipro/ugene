5c4ec8b5dd087f125b9c4944bf2c7781f8a427fb
ichebyki <ichebyki@gmail.com>
2022-11-11 19:41:54 +0700

    Fix scrolling for short sequences

--- src/corelibs/U2View/src/ov_msa/MultilineScrollController.h
+++ src/corelibs/U2View/src/ov_msa/MultilineScrollController.h
@@ -63,8 +63,6 @@ public:
     void scrollToBase(QPoint maPoint);
     void scrollToPoint(const QPoint& maPoint);
 
-    void centerViewRow(QPoint maPoint);
-
     void setMultilineVScrollbarValue(int value);
     void setMultilineVScrollbarBase(int base);
 
@@ -137,11 +135,14 @@ private:
     int savedFirstVisibleMaRow;
     int savedFirstVisibleMaRowOffset;
 
+    bool checkBoundary();
+
     // TODO:ichebyki
     // Deprecated, will be removed
-    void centerBase(int baseNumber);
-    void centerPoint(const QPoint& maPoint);
-    void setCenterVisibleBase(int firstVisibleBase);
+    void _centerBase(int baseNumber);
+    void _centerPoint(const QPoint& maPoint);
+    void _setCenterVisibleBase(int firstVisibleBase);
+    void _centerViewRow(QPoint maPoint);
 };
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(MultilineScrollController::Directions)
