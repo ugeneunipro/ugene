2e3d6abb83e510a7d8e484036f25baf83383e2ad
Yuliya Algaer <yalgaer@gmail.com>
2023-01-23 15:02:39 -0500

    Step 1

--- src/corelibs/U2Core/src/globals/ScriptEngine.h
+++ src/corelibs/U2Core/src/globals/ScriptEngine.h
@@ -1,6 +1,9 @@
 // QScriptEngine is a deprecated QT package and produces warnings during compilation with modern compilers.
 // QT team does not want to update/support this package anymore: so we mute these warnings.
 
+#ifndef _U2_SCRIPT_ENGINE_H_
+#define _U2_SCRIPT_ENGINE_H_
+
 #ifdef __GNUC__
 #    pragma GCC diagnostic push
 #    pragma GCC diagnostic ignored "-Wpragmas"  // Ignore all unknown (for example when an old GCC is used) pragmas below.
@@ -9,4 +12,6 @@
 #include <QScriptEngine>
 #ifdef __GNUC__
 #    pragma GCC diagnostic pop
+#endif
+
 #endif
