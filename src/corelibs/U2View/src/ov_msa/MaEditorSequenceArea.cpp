b8543e61ed711096c833753bd448dc738fb1f18e
Yuliya Algaer <yalgaer@gmail.com>
2023-01-26 12:05:48 -0500

    Fix GUI tests

--- src/corelibs/U2View/src/ov_msa/MaEditorSequenceArea.cpp
+++ src/corelibs/U2View/src/ov_msa/MaEditorSequenceArea.cpp
@@ -841,7 +841,7 @@ void MaEditorSequenceArea::wheelEvent(QWheelEvent* we) {
     // Manually shift scrollbars on wheel event.
     // With no modifiers the default scrollbar is shifted.
     // Wheel + Alt changes the default wheel action from horizontal <=> vertical (default QT behavior for scroll-areas).
-    auto scrollBar = we->modifiers() == Qt::AltModifier ? svBar : (we->modifiers() == 0 ? shBar : nullptr);
+    auto scrollBar = we->modifiers() == Qt::AltModifier ? svBar : (we->modifiers() == 0 ? (shBar->isEnabled() ? shBar : svBar) : nullptr);
     if (scrollBar) {
         scrollBar->triggerAction(toMin ? QAbstractSlider::SliderSingleStepSub : QAbstractSlider::SliderSingleStepAdd);
     }
