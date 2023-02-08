fa9cb0f448f66e6f01d0780271ad93f2b7e860f3
Yuliya Algaer <yalgaer@gmail.com>
2023-02-08 21:00:00 +0700

    UGENE-7720. GUI test & new filler

---
+++ src/plugins/GUITestBase/src/runnables/ugene/plugins_3rdparty/sitecon/SiteconBuildDialogFiller.cpp
@@ -0,0 +1,57 @@
+/**
+* UGENE - Integrated Bioinformatics Tools.
+* Copyright (C) 2008-2023 UniPro <ugene@unipro.ru>
+* http://ugene.net
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+*/
+
+#include "SiteconBuildDialogFiller.h"
+#include <primitives/GTComboBox.h>
+#include <primitives/GTLineEdit.h>
+#include <primitives/GTWidget.h>
+
+#include <QApplication>
+
+namespace U2 {
+using namespace HI;
+
+#define GT_CLASS_NAME "GTUtilsDialog::SiteconBuildDialogFiller"
+
+SiteconBuildDialogFiller::SiteconBuildDialogFiller(HI::GUITestOpStatus& os, const QString& inputFile, const QString& model)
+   : Filler(os, "SiteconBuildDialog"),
+      inputFile(inputFile),
+      model(model) {
+}
+
+SiteconBuildDialogFiller::SiteconBuildDialogFiller(HI::GUITestOpStatus& os, CustomScenario* scenario)
+   : Filler(os, "SiteconBuildDialog", scenario) {
+}
+
+#define GT_METHOD_NAME "commonScenario"
+void SiteconBuildDialogFiller::commonScenario() {
+   QWidget* dialog = GTWidget::getActiveModalWidget(os);
+
+   GTLineEdit::setText(os, "inputEdit", inputFile, dialog);
+   GTLineEdit::setText(os, "outputEdit", model, dialog);
+
+   GTUtilsDialog::clickButtonBox(os, dialog, QDialogButtonBox::Ok);
+}
+#undef GT_METHOD_NAME
+
+#undef GT_CLASS_NAME
+
+}  // namespace U2
--- src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_7001_8000.cpp
+++ src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_7001_8000.cpp
@@ -127,6 +127,7 @@
 #include "runnables/ugene/plugins_3rdparty/MAFFT/MAFFTSupportRunDialogFiller.h"
 #include "runnables/ugene/plugins_3rdparty/kalign/KalignDialogFiller.h"
 #include "runnables/ugene/plugins_3rdparty/primer3/Primer3DialogFiller.h"
+#include "runnables/ugene/plugins_3rdparty/sitecon/SiteconBuildDialogFiller.h"
 #include "runnables/ugene/ugeneui/DocumentFormatSelectorDialogFiller.h"
 #include "runnables/ugene/ugeneui/SaveProjectDialogFiller.h"
 #include "runnables/ugene/ugeneui/SequenceReadingModeSelectorDialogFiller.h"
@@ -4054,6 +4055,14 @@ GUI_TEST_CLASS_DEFINITION(test_7753) {
     GTFileDialog::openFile(os, sandboxFilePath);
     GTUtilsTaskTreeView::waitTaskFinished(os);
     GTUtilsLog::checkContainsError(os, logTracer, QString("File %1 does not exists. Document was removed.").arg(QFileInfo(sandboxFilePath).absoluteFilePath()));
+}
+
+GUI_TEST_CLASS_DEFINITION(test_7770) {
+    GTUtilsDialog::waitForDialog(os, new SiteconBuildDialogFiller(os, testDir + "_common_data/clustal/1000_sequences.aln", "test"));
+    GTMenu::clickMainMenuItem(os, {"Tools", "Search for TFBS", "Build SITECON model..."});
+    GTGlobals::sleep(15000);
+    GTKeyboardDriver::keyClick(Qt::Key_Escape);
+    GTUtilsTaskTreeView::waitTaskFinished(os, 5000);
 }
 
 GUI_TEST_CLASS_DEFINITION(test_7781) {
