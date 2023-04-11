9af1b0c9a0bb4eb4e7c85b09e1a88f7c27c434f7
Yuliya Algaer <yalgaer@gmail.com>
2023-04-11 20:50:08 +0700

    UGENE-7861. GUI test

--- src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_7001_8000.cpp
+++ src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_7001_8000.cpp
@@ -4340,5 +4340,22 @@ GUI_TEST_CLASS_DEFINITION(test_7850) {
                   QString("Bad offset: expected %1, current %2").arg(savedLeftOffset).arg(restoredLeftOffset));
 }
 
+GUI_TEST_CLASS_DEFINITION(test_7861) {
+    // Open COI.aln.
+    GTFileDialog::openFile(os, dataDir + "samples/CLUSTALW/COI.aln");
+    GTUtilsMsaEditor::checkMsaEditorWindowIsActive(os);
+
+    // Press PageDown.
+    GTKeyboardDriver::keyClick(Qt::Key_PageDown);
+
+    // Goto 1.
+    GTUtilsDialog::waitForDialog(os, new GoToDialogFiller(os, 1));
+    GTKeyboardDriver::keyClick('g', Qt::ControlModifier);
+
+    // Expected: position 1 is visible.
+    int leftOffset = GTUtilsMSAEditorSequenceArea::getFirstVisibleBaseIndex(os);
+    CHECK_SET_ERR(leftOffset == 0, QString("Bad offset: expected 0, current %1").arg(leftOffset));
+}
+
 }  // namespace GUITest_regression_scenarios
 }  // namespace U2
