77815b4ada1bded3047772d0af0fa0ea420a9b8d
Kirill Rasputin <rasputinkirill@gmail.com>
2023-04-17 20:37:43 +0700

    update

--- src/corelibs/U2View/src/ov_sequence/sequence_info/SequenceInfo.cpp
+++ src/corelibs/U2View/src/ov_sequence/sequence_info/SequenceInfo.cpp
@@ -605,8 +605,8 @@ void SequenceInfo::launchCalculations(const QString& subgroupId) {
     }
 
     if (subgroupId.isEmpty() || subgroupId == CODON_OCCUR_GROUP_ID || subgroupId == AMINO_ACID_OCCUR_GROUP_ID) {
-        bool isCodonReportOpened = codonWidget->isVisible() && codonWidget->isSubgroupOpened();
-        bool isAminoAcidReportOpened = aminoAcidWidget->isSubgroupOpened();
+        bool isCodonReportOpened = activeContext->getAlphabet()->isNucleic() && codonWidget->isSubgroupOpened();
+        bool isAminoAcidReportOpened = aminoAcidWidget->isVisible() && aminoAcidWidget->isSubgroupOpened();
         if (isCodonReportOpened || isAminoAcidReportOpened) {
             if (isCodonReportOpened) {
                 codonWidget->showProgress();
