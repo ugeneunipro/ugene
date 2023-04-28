c9a0afa0bb1fb234d56ccb967416ed1c8ae24a69
Dmitrii Sukhomlinov <dsukhomlinov@unipro.ru>
2023-04-28 13:20:00 +0700

    UGENE-7785 update

--- src/plugins/pcr/src/ExtractProductTask.cpp
+++ src/plugins/pcr/src/ExtractProductTask.cpp
@@ -247,7 +247,6 @@ void ExtractProductTask::run() {
     annotations->addAnnotations(QList<SharedAnnotationData>() << getPrimerAnnotation(product.reversePrimerMatchLength, U2Strand::Complementary, productSequence.length()));
     annotations->addObjectRelation(GObjectRelation(GObjectReference(sequenceObject), ObjectRole_Sequence));
     doc->addObject(annotations);
-    doc->getGHints()->set(DocumentRemovalMode_Synchronous, true);
 
     if (settings.annotationsExtraction != ExtractProductSettings::None) {
         for (const U2EntityRef& annsRef : qAsConst(settings.annotationRefs)) {
