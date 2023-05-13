9b872ae61fc3e73917e9c3190e5fc97f36f153bd
Yuliya Algaer <yalgaer@gmail.com>
2023-05-13 04:04:41 -0400

    Add restore_dirs_before_test.sh to Mac OS GUI tests

--- etc/script/mac/run_gui_tests.sh
+++ etc/script/mac/run_gui_tests.sh
@@ -7,8 +7,10 @@
 
 # ============== Environment for test script
 TEAMCITY_WORK_DIR=$(pwd)
-UGENE_DIR="${TEAMCITY_WORK_DIR}/ugene_app/Unipro UGENE.app/Contents/MacOS"
 echo "TEAMCITY_WORK_DIR: '${TEAMCITY_WORK_DIR}', UGENE_DIR: '${UGENE_DIR}'"
+UGENE_DIR="${TEAMCITY_WORK_DIR}/ugene_app/Unipro UGENE.app/Contents/MacOS"
+SOURCE_DIR="${TEAMCITY_WORK_DIR}/ugene_git"
+SCRIPTS_DIR="${SOURCE_DIR}/etc/script/linux"
 
 if [ -z "${UGENE_USE_DIRECT_API_TO_OPEN_FILES}" ]; then export UGENE_USE_DIRECT_API_TO_OPEN_FILES="1"; fi
 if [ -z "${UGENE_USE_MASTER_USER_INI}" ]; then export UGENE_USE_MASTER_USER_INI="1"; fi
