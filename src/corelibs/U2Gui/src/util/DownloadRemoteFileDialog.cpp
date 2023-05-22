d625444733c613b275dd10ac4c1d0365612873af
Dmitrii Sukhomlinov <dsukhomlinov@unipro.ru>
2023-05-22 13:11:32 +0700

    UGENE-7834 Add link to database in the Access remote databases... dialog

--- src/corelibs/U2Gui/src/util/DownloadRemoteFileDialog.cpp
+++ src/corelibs/U2Gui/src/util/DownloadRemoteFileDialog.cpp
@@ -47,6 +47,8 @@
 
 static const QString SAVE_DIR("downloadremotefiledialog/savedir");
 static const QString HINT_STYLE_SHEET = "color:green; font:bold";
+static const QString DB_EXTERNAL_LINK = QString(R"(<a href="%1"><img src=":core/images/external_link.png" width="22" height="22";></a>)");
+
 
 namespace U2 {
 
@@ -65,6 +67,10 @@ DownloadRemoteFileDialog::DownloadRemoteFileDialog(QWidget* p)
     adjustSize();
 
     RemoteDBRegistry& registry = RemoteDBRegistry::getRemoteDBRegistry();
+    connect(ui->databasesBox, &QComboBox::currentTextChanged, this, [this, &registry](const QString& text) {
+        ui->lbExternalLink->setText(DB_EXTERNAL_LINK.arg(registry.getExternalLinkByName(text)));
+    });
+
     const QList<QString> dataBases = registry.getDBs();
     foreach (const QString& dbName, dataBases) {
         ui->databasesBox->addItem(dbName, dbName);
