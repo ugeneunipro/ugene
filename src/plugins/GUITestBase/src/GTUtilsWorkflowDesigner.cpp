32ad1e3b7a7db7570835411a34a1d0fd4bcc901a
Yuliya Algaer <yalgaer@gmail.com>
2023-06-03 10:26:08 -0500

    Fix more test issues

--- src/plugins/GUITestBase/src/GTUtilsWorkflowDesigner.cpp
+++ src/plugins/GUITestBase/src/GTUtilsWorkflowDesigner.cpp
@@ -175,7 +175,7 @@ void GTUtilsWorkflowDesigner::stopWorkflow(HI::GUITestOpStatus& os) {
 
 #define GT_METHOD_NAME "returnToWorkflow"
 void GTUtilsWorkflowDesigner::returnToWorkflow(HI::GUITestOpStatus& os) {
-    GTWidget::click(os, GTAction::button(os, GTAction::findActionByText(os, "To Workflow Designer")));
+    GTWidget::click(os, getGotoWorkflowButton(os));
 }
 #undef GT_METHOD_NAME
 
