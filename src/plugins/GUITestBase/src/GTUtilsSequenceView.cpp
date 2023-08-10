88f5d405edb66af95e5fda7a04bc1f402bca3a70
Yuliya Algaer <yalgaer@gmail.com>
2023-08-09 20:13:44 -0500

    Fix GUI test 6685_5 on macOs

--- src/plugins/GUITestBase/src/GTUtilsSequenceView.cpp
+++ src/plugins/GUITestBase/src/GTUtilsSequenceView.cpp
@@ -440,7 +440,7 @@ void GTUtilsSequenceView::clickAnnotationDet(
     }
 }
 
-void GTUtilsSequenceView::clickAnnotationPan(const QString& name, int startPos, int number, const bool isDoubleClick, Qt::MouseButton button) {
+void GTUtilsSequenceView::clickAnnotationPan(const QString& name, int startPos, int number, bool isDoubleClick, Qt::MouseButton button) {
     ADVSingleSequenceWidget* seq = getSeqWidgetByNumber(number);
     GSequenceLineViewRenderArea* area = seq->getPanView()->getRenderArea();
     auto pan = dynamic_cast<PanViewRenderArea*>(area);
@@ -479,8 +479,7 @@ void GTUtilsSequenceView::clickAnnotationPan(const QString& name, int startPos, 
         GTGlobals::sleep();
     }
 
-    const U2Region visibleLocation = r.intersect(vr);
-
+    U2Region visibleLocation = r.intersect(vr);
     U2Region y = pan->getAnnotationYRange(a, 0, as);
 
     float start = visibleLocation.startPos;
@@ -491,7 +490,7 @@ void GTUtilsSequenceView::clickAnnotationPan(const QString& name, int startPos, 
     int rw = qMax(MIN_ANNOTATION_WIDTH, qRound(x2f - x1f));
     int x1 = qRound(x1f);
 
-    const QRect annotationRect(x1, y.startPos, rw, y.length);
+    QRect annotationRect(x1, y.startPos, rw, y.length);
     GTMouseDriver::moveTo(pan->mapToGlobal(annotationRect.center()));
     if (isDoubleClick) {
         GTMouseDriver::doubleClick();
