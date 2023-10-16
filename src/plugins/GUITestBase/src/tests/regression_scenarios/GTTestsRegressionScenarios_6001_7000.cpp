22053cbc482b38e2f71a52701bc2b0dde4418a9d
Yuliya Algaer <yalgaer@gmail.com>
2023-10-16 23:13:33 +0700

    UGENE-6321. GUI test

--- src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_6001_7000.cpp
+++ src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_6001_7000.cpp
@@ -1753,6 +1753,42 @@ GUI_TEST_CLASS_DEFINITION(test_6314) {
                       .arg(name.size()));
 }
 
+GUI_TEST_CLASS_DEFINITION(test_6321) {
+    GTFileDialog::openFile(testDir + "_common_data/fasta/", "AMINO.fa");
+    GTUtilsTaskTreeView::waitTaskFinished();
+
+    GTUtilsSequenceView::selectSequenceRegion(10, 20);
+
+    class CreateAnnotationScenario : public CustomScenario {
+    public:
+        void run() override {
+            QWidget* dialog = GTWidget::getActiveModalWidget();
+
+            CHECK_SET_ERR(GTWidget::findWidget("chbComplement", nullptr, {false}) == nullptr, "chbComplement is shown");
+            CHECK_SET_ERR(GTWidget::findWidget("tbDoComplement", nullptr, {false}) == nullptr, "tbDoComplement is shown");
+
+            GTUtilsDialog::clickButtonBox(dialog, QDialogButtonBox::Ok);
+        }
+    };
+
+    GTUtilsDialog::waitForDialog(new CreateAnnotationWidgetFiller(new CreateAnnotationScenario));
+    GTToolbar::clickButtonByTooltipOnToolbar(MWTOOLBAR_ACTIVEMDI, "New annotation");
+
+    class EditAnnotationScenario : public CustomScenario {
+    public:
+        void run() override {
+
+            CHECK_SET_ERR(GTWidget::findWidget("chbComplement", nullptr, {false}) == nullptr, "chbComplement is shown");
+            CHECK_SET_ERR(GTWidget::findWidget("tbDoComplement", nullptr, {false}) == nullptr, "tbDoComplement is shown");
+            GTUtilsDialog::clickButtonBox(QDialogButtonBox::Ok);
+        }
+    };
+
+    GTUtilsDialog::waitForDialog(new EditAnnotationFiller(new EditAnnotationScenario()));
+    GTKeyboardDriver::keyClick(Qt::Key_F2);
+    GTTreeWidget::click(GTUtilsAnnotationsTreeView::findItem("misc_feature"));
+}
+
 GUI_TEST_CLASS_DEFINITION(test_6350) {
     // 1. Open "human_T1.fa"
     GTFileDialog::openFile(dataDir + "samples/FASTA/human_T1.fa");
