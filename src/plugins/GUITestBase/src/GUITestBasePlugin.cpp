b7031d9f2e8a402317fd1ac5a9c613578c87a7f3
Dmitrii Sukhomlinov <sukhomlinovdmitrii@yandex.ru>
2023-11-21 10:17:13 +0700

    UGENE-7959 Add "BLAST selected primer pairs" option (#1458)
    * UGENE-7959 Add BLAST for primer pairs option
    
    * UGENE-7959 Add BLAST selected primer pairs option
    
    * UGENE-7959 build fix
    
    * UGENE-7959 update
    
    * UGENE-7959 eofs update
    
    * UGENE-7959 test eof update
    
    * UGENE-7959 comment update
    
    * UGENE-7959 translations update
    
    * UGENE-7959 Review fix

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -2059,6 +2059,7 @@ void GUITestBasePlugin::registerTests(UGUITestBase* guiTestBase) {
     REGISTER_TEST(GUITest_common_scenarios_sequence_view::test_0083);
     REGISTER_TEST(GUITest_common_scenarios_sequence_view::test_0084);
     REGISTER_TEST(GUITest_common_scenarios_sequence_view::test_0085);
+    REGISTER_TEST(GUITest_common_scenarios_sequence_view::test_0086);
 
     //////////////////////////////////////////////////////////////////////////
     // Common scenarios/sequence edit/
