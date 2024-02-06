2f85b9143373e291ea19af34d15b98554ba3e76d
Dmitrii Sukhomlinov <sukhomlinovdmitrii@yandex.ru>
2024-02-06 09:32:53 +0700

    UGENE-7482 [Sequence] Crash when copying 2 GB sequence (#1539)
    * UGENE-7484 [Sequence] Crash when copying 2 GB sequence
    
    * UGENE-7482 translations update
    
    * UGENE-7482 GUI test update

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -1739,6 +1739,7 @@ void GUITestBasePlugin::registerTests(UGUITestBase* guiTestBase) {
     REGISTER_TEST(GUITest_regression_scenarios::test_7473_1);
     REGISTER_TEST(GUITest_regression_scenarios::test_7473_2);
     REGISTER_TEST(GUITest_regression_scenarios::test_7476);
+    REGISTER_TEST(GUITest_regression_scenarios::test_7482);
     REGISTER_TEST(GUITest_regression_scenarios::test_7487_1);
     REGISTER_TEST(GUITest_regression_scenarios::test_7487_2);
     REGISTER_TEST(GUITest_regression_scenarios::test_7490);
