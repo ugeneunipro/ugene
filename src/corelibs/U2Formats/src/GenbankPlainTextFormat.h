e637ea7ac3553b7385ff2c5a9428afe074609146
Yuliya Algaer <yalgaer@gmail.com>
2024-03-09 05:03:42 +0700

    UGENE-8049. Error in annotating circular on GenBank LOCUS line (#1571)
    * UGENE-8049. Error in annotating circular on GenBank LOCUS line
    
    * Cleanup
    
    * Fix build
    
    * Add GUI test
    
    * Fix gui tests

--- src/corelibs/U2Formats/src/GenbankPlainTextFormat.h
+++ src/corelibs/U2Formats/src/GenbankPlainTextFormat.h
@@ -32,38 +32,38 @@ class U2FORMATS_EXPORT GenbankPlainTextFormat : public EMBLGenbankAbstractDocume
 public:
     GenbankPlainTextFormat(QObject* p);
 
-    virtual void storeDocument(Document* doc, IOAdapter* io, U2OpStatus& os);
+    void storeDocument(Document* doc, IOAdapter* io, U2OpStatus& os) override;
 
-    virtual bool isStreamingSupport();
+    bool isStreamingSupport() override;
 
-    virtual void storeEntry(IOAdapter* io, const QMap<GObjectType, QList<GObject*>>& objectsMap, U2OpStatus& os);
+    void storeEntry(IOAdapter* io, const QMap<GObjectType, QList<GObject*>>& objectsMap, U2OpStatus& os) override;
 
     static bool checkCircularity(const GUrl& filePath, U2OpStatus& os);
 
 protected:
-    virtual FormatCheckResult checkRawTextData(const QByteArray& rawData, const GUrl& = GUrl()) const;
+    FormatCheckResult checkRawTextData(const QByteArray& rawData, const GUrl& = GUrl()) const override;
 
     typedef QPair<QString, QString> StrPair;
 
     static bool readIdLine(ParserState*);
-    bool readEntry(ParserState*, U2SequenceImporter&, int& seqSize, int& fullSeqSize, bool merge, int gapSize, U2OpStatus&);
-    void readHeaderAttributes(QVariantMap& tags, DbiConnection& con, U2SequenceObject* so);
+    bool readEntry(ParserState*, U2SequenceImporter&, int& seqSize, int& fullSeqSize, bool merge, int gapSize, U2OpStatus&) override;
+    void readHeaderAttributes(QVariantMap& tags, DbiConnection& con, U2SequenceObject* so) override;
 
     void writeAnnotations(IOAdapter* io, const QList<GObject*>& aos, U2OpStatus& si);
-    QString genLocusString(const QList<GObject*>& aos, U2SequenceObject* so, const QString& locusStrFromAttr);
-    bool writeKeyword(IOAdapter* io, U2OpStatus& os, const QString& key, const QString& value, bool wrap = true /*TODO*/);
-    void writeSequence(IOAdapter* io, U2SequenceObject* ao, const QList<U2Region>& lowerCaseRegs, U2OpStatus& si);
-    void prepareMultiline(QString& line, int spacesOnLineStart, bool lineBreakOnlyOnSpace = true, bool newLineAtTheEnd = true, int maxLineLen = 79);
+    static QString genLocusString(const QList<GObject*>& aos, U2SequenceObject* so, const QString& locusStrFromAttr);
+    static bool writeKeyword(IOAdapter* io, U2OpStatus& os, const QString& key, const QString& value);
+    static void writeSequence(IOAdapter* io, U2SequenceObject* ao, const QList<U2Region>& lowerCaseRegs, U2OpStatus& si);
+    static void prepareMultiline(QString& line, int spacesOnLineStart, bool lineBreakOnlyOnSpace = true, bool newLineAtTheEnd = true, int maxLineLen = 79);
     void writeQualifier(const QString& name, const QString& val, IOAdapter* io, U2OpStatus& si, const char* spaceLine);
     QString prepareQualifierSingleString(const QString& qualifierName, const QString& qualifierValue) const;
     QList<StrPair> formatKeywords(const QVariantMap& varMap, bool withLocus = false);
     virtual QList<StrPair> processCommentKeys(QMultiMap<QString, QVariant>& tags);
 
-    bool isNcbiLikeFormat() const;
-    void createCommentAnnotation(const QStringList& comments, int sequenceLength, AnnotationTableObject* annTable) const;
-    U2FeatureType getFeatureType(const QString& typeString) const;
-    QString getFeatureTypeString(U2FeatureType featureType, bool isAmino) const;
-    bool breakQualifierOnSpaceOnly(const QString& qualifierName) const;
+    bool isNcbiLikeFormat() const override;
+    void createCommentAnnotation(const QStringList& comments, int sequenceLength, AnnotationTableObject* annTable) const override;
+    U2FeatureType getFeatureType(const QString& typeString) const override;
+    static QString getFeatureTypeString(U2FeatureType featureType, bool isAmino) ;
+    bool breakQualifierOnSpaceOnly(const QString& qualifierName) const override;
 
     static constexpr int VAL_OFF = 12;
 
