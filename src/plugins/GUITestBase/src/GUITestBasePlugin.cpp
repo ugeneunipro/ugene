469fcabbbc58421f47cd3413f6e421611cfaaa81
Kirill Rasputin <rasputinkirill@gmail.com>
2024-03-18 23:52:45 +0700

    isolate test

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -179,6 +179,8 @@ void GUITestBasePlugin::sl_showWindow() {
 void GUITestBasePlugin::registerTests(UGUITestBase* guiTestBase) {
     SAFE_POINT(guiTestBase != nullptr, "guiTestBase is null!", );
 
+    REGISTER_TEST(GUITest_regression_scenarios::test_6485);
+    return;
     //////////////////////////////////////////////////////////////////////////
     // Tests for investigation
     //////////////////////////////////////////////////////////////////////////
