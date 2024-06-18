759570eab8dd6d241970eceaa729044bd1dab4cb
DmitriiSukhomlinov <sukhomlinovdmitrii@yandex.ru>
2024-06-18 11:28:57 +0700

    UGENE-7610 Linux build fix

--- src/ugeneui/src/main_window/styles/ProxyStyle.cpp
+++ src/ugeneui/src/main_window/styles/ProxyStyle.cpp
@@ -13,8 +13,6 @@ ProxyStyle::ProxyStyle(QStyle* style)
 void ProxyStyle::polish(QWidget* widget) {
     QProxyStyle::polish(widget);
     QFont defaultFont = QApplication::font();
-    auto ps = defaultFont.pointSize();
-
 
 #if QT_VERSION_CHECK(5, 10, 0) < QT_VERSION
     // It's a workaround for https://bugreports.qt.io/browse/QTBUG-49435:
