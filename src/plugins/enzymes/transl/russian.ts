e3121ca38b23d7c805a7ad720718d4034e43eab8
Kirill Rasputin <rasputikirill@gmail.com>
2024-09-06 01:36:18 +0700

    translation update

--- src/plugins/enzymes/transl/russian.ts
+++ src/plugins/enzymes/transl/russian.ts
@@ -1398,7 +1398,7 @@ To start ligation create a project or open an existing.</source>
     <message>
         <location filename="../src/FindEnzymesTask.cpp" line="300"/>
         <source>The following enzymes were found, but skipped because they were found inside of the &quot;Uncut area&quot;: %1.</source>
-        <translation>Некоторые ферменты были найдены, но опущены потомучто они находились внутри &quot;Области исключения&quot;: %1.</translation>
+        <translation>Некоторые ферменты были найдены, но опущены потому что они находились внутри &quot;Области исключения&quot;: %1.</translation>
     </message>
     <message>
         <location filename="../src/FindEnzymesTask.cpp" line="222"/>
