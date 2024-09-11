ab3bd2acc06c5436928e1e97b239fd3d453d55bc
Yuliya Algaer <yalgaer@gmail.com>
2024-09-11 04:56:13 -0500

    Cloud Workspace: Improve error message

--- src/ugeneui/src/workspace/WebSocketClientService.cpp
+++ src/ugeneui/src/workspace/WebSocketClientService.cpp
@@ -129,7 +129,7 @@ void WebSocketClientService::onTextMessageReceived(const QString& message) {
 }
 
 void WebSocketClientService::onError(QAbstractSocket::SocketError error) {
-    ioLog.trace("WebSocket error:" + error);
+    ioLog.trace("WebSocket error:" + QString::number(error));
     CHECK_EXT(!isRetrying, ioLog.trace("WebSocketClientService::onError - won't retry when inside refreshWebSocketConnection"), );
     isRetrying = true;
     QTimer::singleShot(3000, this, [this] {
