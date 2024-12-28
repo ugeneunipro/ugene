928281cb0bf184e5ba39d9cce458cefc907f00a8
Yuliya Algaer <yalgaer@gmail.com>
2024-12-27 20:50:14 -0600

    Reformat

--- cmake/CollectTranslations.cmake
+++ cmake/CollectTranslations.cmake
@@ -1,7 +1,7 @@
-# Collect translations for Russian
+# Collect translations for Russian.
 file(GLOB PROJECT_TRANSLATIONS_RU transl/russian.ts)
 set(GLOBAL_TRANSLATIONS_RU ${GLOBAL_TRANSLATIONS_RU} ${PROJECT_TRANSLATIONS_RU} PARENT_SCOPE)
 
-# Collect translations for Turkish
+# Collect translations for Turkish.
 file(GLOB PROJECT_TRANSLATIONS_TR transl/turkish.ts)
 set(GLOBAL_TRANSLATIONS_TR ${GLOBAL_TRANSLATIONS_TR} ${PROJECT_TRANSLATIONS_TR} PARENT_SCOPE)
