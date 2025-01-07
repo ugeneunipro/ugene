e40d4042d1fbb26b465c958d1c2e0142ce7dfcff
Yuliya Algaer <yalgaer@gmail.com>
2025-01-06 23:21:41 -0600

    Switch build.sh on Linux to CMake (#1707)
    * Switch build.sh on Linux to CMake
    
    * Update .gitignore
    
    * Better build.sh
    
    * Improve cleaning
    
    * Fix typo
    
    * Check GCC level for warnings
    
    * Optimize warnings per gcc version
    
    * Disable warnings checks
    
    * Add gcc version check
    
    * Fix dist dir
    
    * Fix license file location
    
    * Fix bundle content
    
    * Fix ugenedb library build
    
    * Fix OpenGL tests

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -1,7 +1,3 @@
-if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(UGENE_BUILD_MODE_SUFFIX "d")
-else ()
-    set(UGENE_BUILD_MODE_SUFFIX "")
-endif ()
+include(${CMAKE_SOURCE_DIR}/cmake/Environment.cmake)
 
 include(${CMAKE_SOURCE_DIR}/cmake/CollectTranslations.cmake)
--- etc/script/linux/build.sh
+++ etc/script/linux/build.sh
@@ -17,7 +17,13 @@ fi
 
 TEAMCITY_WORK_DIR=$(pwd)
 UGENE_DIR="${TEAMCITY_WORK_DIR}/ugene"
+#UGENE_DIR="${TEAMCITY_WORK_DIR}"
 SCRIPTS_DIR="${UGENE_DIR}/etc/script/linux"
+BUILD_DIR="${UGENE_DIR}/cmake-build-release"
+DIST_DIR="${BUILD_DIR}/dist"
+
+# Needed by CMake.
+export Qt5_DIR="${QT_DIR}"
 
 # Below this point the script works in 'UGENE_DIR' folder.
 cd "${UGENE_DIR}" || {
@@ -26,65 +32,59 @@ cd "${UGENE_DIR}" || {
 }
 
 if [ -z "${UGENE_BUILD_AND_TEST_SKIP_CLEAN}" ]; then UGENE_BUILD_AND_TEST_SKIP_CLEAN="0"; fi
-if [ -z "${UGENE_BUILD_AND_TEST_SKIP_QMAKE}" ]; then UGENE_BUILD_AND_TEST_SKIP_QMAKE="0"; fi
-if [ -z "${UGENE_BUILD_AND_TEST_SKIP_MAKE}" ]; then UGENE_BUILD_AND_TEST_SKIP_MAKE="0"; fi
-
-MAKE_TARGET=release
-BUILD_DIR="${UGENE_DIR}/src/_release"
 
 ##### Clean ####
 if [ "${UGENE_BUILD_AND_TEST_SKIP_CLEAN}" -eq "1" ]; then
-  echo "skip clean"
+  echo "Skipping clean"
 elif [ "${UGENE_BUILD_AND_TEST_SKIP_CLEAN}" -eq "2" ]; then
   echo "##teamcity[blockOpened name='fast clean']"
   rm -rf "${BUILD_DIR}"
   echo "##teamcity[blockClosed name='fast clean']"
 else
   echo "##teamcity[blockOpened name='make clean']"
-  make clean
+  if
+      cmake --build "${BUILD_DIR}" --target clean
+    then
+      echo "Clean completed successfully"
+    else
+      echo "##teamcity[buildStatus status='FAILURE' text='{build.status.text}. cmake clean failed']"
+      exit 1
+    fi
   echo "##teamcity[blockClosed name='make clean']"
 fi
 
-#### QMake ####
-if [ "${UGENE_BUILD_AND_TEST_SKIP_QMAKE}" -ne "1" ]; then
-  echo "##teamcity[blockOpened name='qmake -r ugene.pro']"
-  if
-    # shellcheck disable=SC2086
-    "${QT_DIR}/bin/qmake" -r ugene.pro ${UGENE_QMAKE_PARAMS}
-  then
-    echo
-  else
-    echo "##teamcity[buildStatus status='FAILURE' text='{build.status.text}. qmake -r ugene.pro failed']"
-    exit 1
-  fi
-  echo "##teamcity[blockClosed name='qmake -r ugene.pro']"
+#### CMake ####
+echo "##teamcity[blockOpened name='CMake']"
+if
+  cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" -S "${UGENE_DIR}" -B "${BUILD_DIR}"
+then
+  echo "CMake finished successfully"
+else
+  echo "##teamcity[buildStatus status='FAILURE' text='{build.status.text}. CMake failed']"
+  exit 1
 fi
+echo "##teamcity[blockClosed name='CMake']"
 
 #### Make ####
-if [ "${UGENE_BUILD_AND_TEST_SKIP_MAKE}" -ne "1" ]; then
-  UGENE_MAKE_PARAMS="-j$(grep "cpu cores" /proc/cpuinfo | uniq | cut -d ':' -f2) ${MAKE_TARGET}"
-  echo "##teamcity[blockOpened name='make ${UGENE_MAKE_PARAMS}']"
-  if
-    # We want these params to be individual params, so disabling inspection for quotes.
-    # shellcheck disable=SC2086
-    make ${UGENE_MAKE_PARAMS}
-  then
-    echo
-  else
-    echo "##teamcity[buildStatus status='FAILURE' text='{build.status.text}. make ${UGENE_MAKE_PARAMS} failed']"
-    exit 1
-  fi
-  echo "##teamcity[blockClosed name='make ${UGENE_MAKE_PARAMS}']"
+UGENE_MAKE_PARAMS="-j$(grep "cpu cores" /proc/cpuinfo | uniq | cut -d ':' -f2)"
+echo "##teamcity[blockOpened name='make ${UGENE_MAKE_PARAMS}']"
+if
+  # We want these params to be individual params, so disabling inspection for quotes.
+  # shellcheck disable=SC2086
+  cmake --build "${BUILD_DIR}" --target all -- -j ${UGENE_MAKE_PARAMS}
+then
+  echo
+else
+  echo "##teamcity[buildStatus status='FAILURE' text='{build.status.text}. make ${UGENE_MAKE_PARAMS} failed']"
+  exit 1
 fi
+echo "##teamcity[blockClosed name='make ${UGENE_MAKE_PARAMS}']"
 
 #### BUNDLE ####
 echo "##teamcity[blockOpened name='Bundle']"
 
-# Remove not needed build artifacts like static libs.
-rm -f "${BUILD_DIR}"/*.a
-
 # Copy & patch Qt libs.
-"${SCRIPTS_DIR}/copy_qt_files_to_ugene.sh" "${QT_DIR}" "${BUILD_DIR}" || {
+"${SCRIPTS_DIR}/copy_qt_files_to_ugene.sh" "${QT_DIR}" "${DIST_DIR}" || {
   echo "'copy_qt_files_to_ugene.sh' script failed"
   exit 1
 }
@@ -93,32 +93,32 @@ rm -f "${BUILD_DIR}"/*.a
 
 # Update RPATH for all shared libraries in the plugins directory.
 # shellcheck disable=SC2016
-find "${BUILD_DIR}/plugins" -name "*.so" -exec patchelf --force-rpath --set-rpath '$ORIGIN/..' {} \;
+find "${DIST_DIR}/plugins" -name "*.so" -exec patchelf --force-rpath --set-rpath '$ORIGIN/..' {} \;
 
 # Update RPATH for all shared libraries in the build directory.
 # shellcheck disable=SC2016
-find "${BUILD_DIR}" -maxdepth 1 -name "*.so" -exec patchelf --force-rpath --set-rpath '$ORIGIN' {} \;
+find "${DIST_DIR}" -maxdepth 1 -name "*.so" -exec patchelf --force-rpath --set-rpath '$ORIGIN' {} \;
 
 # Update RPATH for individual binaries.
 # shellcheck disable=SC2016
-patchelf --force-rpath --set-rpath '$ORIGIN' "${BUILD_DIR}/plugins_checker"
+patchelf --force-rpath --set-rpath '$ORIGIN' "${DIST_DIR}/plugins_checker"
 # shellcheck disable=SC2016
-patchelf --force-rpath --set-rpath '$ORIGIN' "${BUILD_DIR}/ugenecl"
+patchelf --force-rpath --set-rpath '$ORIGIN' "${DIST_DIR}/ugenecl"
 # shellcheck disable=SC2016
-patchelf --force-rpath --set-rpath '$ORIGIN' "${BUILD_DIR}/ugenem"
+patchelf --force-rpath --set-rpath '$ORIGIN' "${DIST_DIR}/ugenem"
 # shellcheck disable=SC2016
-patchelf --force-rpath --set-rpath '$ORIGIN' "${BUILD_DIR}/ugeneui"
+patchelf --force-rpath --set-rpath '$ORIGIN' "${DIST_DIR}/ugeneui"
 
 # Add data.
-rm -rf "${BUILD_DIR}/data"
-cp -r ./data "${BUILD_DIR}"
+rm -rf "${DIST_DIR}/data"
+cp -r ./data "${DIST_DIR}"
 
-cp "${UGENE_DIR}/etc/shared/ugene.png" "${BUILD_DIR}/"
-cp "${UGENE_DIR}/LICENSE.txt" "${BUILD_DIR}/"
-cp "${UGENE_DIR}/LICENSE.3rd_party.txt" "${BUILD_DIR}/"
+cp "${UGENE_DIR}/etc/shared/ugene.png" "${DIST_DIR}/"
+cp "${UGENE_DIR}/LICENSE.txt" "${DIST_DIR}/"
+cp "${UGENE_DIR}/LICENSE.3rd_party.txt" "${DIST_DIR}/"
 
 echo "Compressing app into a tar.gz"
 rm "${TEAMCITY_WORK_DIR}/"*.gz
-tar cfz "${TEAMCITY_WORK_DIR}/bundle-linux-b${TEAMCITY_BUILD_COUNTER}.tar.gz" -C "${BUILD_DIR}" .
+tar cfz "${TEAMCITY_WORK_DIR}/bundle-linux-b${TEAMCITY_BUILD_COUNTER}.tar.gz" -C "${DIST_DIR}" .
 
 echo "##teamcity[blockClosed name='Bundle']"
