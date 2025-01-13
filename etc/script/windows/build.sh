da41e46a52119478ebecc526cd1ed64ce371ebdb
Yuliya Algaer <yalgaer@gmail.com>
2025-01-12 16:00:39 -0800

    Fix Visual Studio generator dirs

--- etc/script/windows/build.sh
+++ etc/script/windows/build.sh
@@ -55,7 +55,7 @@ fi
 echo "##teamcity[blockOpened name='CMake']"
 if
 #  cmake -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" -S "${UGENE_DIR}" -B "${BUILD_DIR}"
-  cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 16 2019" -S "${UGENE_DIR}" -B "${BUILD_DIR}"
+  cmake -DCMAKE_CONFIGURATION_TYPES=Release -G "Visual Studio 16 2019" -S "${UGENE_DIR}" -B "${BUILD_DIR}"
 then
   echo "CMake finished successfully"
 else
@@ -68,7 +68,7 @@ echo "##teamcity[blockOpened name='make']"
 if
   # We want these params to be individual params, so disabling inspection for quotes.
   # shellcheck disable=SC2086
-  cmake --build "${BUILD_DIR}" --parallel
+  cmake --build "${BUILD_DIR}" --parallel --config Release
 then
   echo
 else
