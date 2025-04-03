1ed6b2c07d0147eaaa17b2122948bef564c1c163
Kirill Rasputin <rasputikirill@gmail.com>
2025-04-03 07:50:15 +0700

    build fix

--- src/plugins/dbi_bam/src/Dbi.cpp
+++ src/plugins/dbi_bam/src/Dbi.cpp
@@ -616,6 +616,8 @@ qint64 AssemblyDbi::countReads(const U2DataId& assemblyId, const U2Region& r, U2
 }
 
 U2DbiIterator<U2AssemblyRead>* AssemblyDbi::getReads(const U2DataId& assemblyId, const U2Region& r, U2OpStatus& os, bool sortedHint, bool readsStrictlyFitRegion) {
+    Q_UNUSED(sortedHint)
+    Q_UNUSED(readsStrictlyFitRegion)
     try {
         if (U2DbiState_Ready != dbi.getState()) {
             throw Exception(BAMDbiPlugin::tr("Invalid DBI state"));
