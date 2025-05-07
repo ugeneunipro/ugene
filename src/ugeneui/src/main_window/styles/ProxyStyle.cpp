0177b0604d8a5220830d62bc9ac15efffd962e19
Dmitrii Sukhomlinov <dsukhomlinov@unipro.ru>
2025-05-07 16:19:10 +0700

    UGENE-7610 Dark Style engine

--- src/ugeneui/src/main_window/styles/ProxyStyle.cpp
+++ src/ugeneui/src/main_window/styles/ProxyStyle.cpp
@@ -2,6 +2,8 @@
 
 #include <QMenu>
 
+#include <QApplication>
+
 namespace U2 {
 
 ProxyStyle::ProxyStyle(QStyle* style)
@@ -10,6 +12,7 @@ ProxyStyle::ProxyStyle(QStyle* style)
 
 void ProxyStyle::polish(QWidget* widget) {
     QProxyStyle::polish(widget);
+    QFont defaultFont = QApplication::font();
 
 #if QT_VERSION_CHECK(5, 10, 0) < QT_VERSION
     // It's a workaround for https://bugreports.qt.io/browse/QTBUG-49435:
