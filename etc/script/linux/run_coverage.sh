5ed82e6302dae5c8a862c78a98ad3751e0300376
Dmitrii Sukhomlinov <dsukhomlinov@unipro.ru>
2025-07-23 18:43:25 +0700

    Fix coverage script and temporary disable XML tests

--- etc/script/linux/run_coverage.sh
+++ etc/script/linux/run_coverage.sh
@@ -8,12 +8,12 @@ echo "##teamcity[blockOpened name='Build UGENE']"
 echo "##teamcity[blockClosed name='Build UGENE']"
 
 echo "##teamcity[blockOpened name='Run XML Tests']"
-"${SCRIPTS_DIR}"/run_xml_tests.sh
+#"${SCRIPTS_DIR}"/run_xml_tests.sh
 echo "##teamcity[blockClosed name='Run XML Tests']"
 
 echo "##teamcity[blockOpened name='Run Coverage']"
 
-"${LCOV_DIR}"lcov --capture --directory ./ugene/cmake-build-debug --output-file coverage.info
+"${LCOV_DIR}"lcov --capture --directory ./ugene/cmake-build-relwithcov --output-file coverage.info
 "${LCOV_DIR}"lcov --remove coverage.info "/usr/*" "/home/ugene/Qt/*" "*_autogen*" "*qrc_*" "*breakpad*" "*qscore/src/qscore*" "*QSpec*" "*samtools/src/samtools_core*" "*sqlite*" "*qtscript*" "*zlib*" "*plugins/api_tests*" "*plugins/GUITestBase*" "*ball/src/include*" "*ball/src/source*" "*gor4/src/gor.*" "*gor4/src/nrutil.*" "*hmm2/src/hmmer2*" "*phylip/src/neighbor.h" "*phylip/src/phylip.*" "*phylip/src/protdist.*" "*phylip/src/seq.*" "*phylip/src/seqboot.*" "*primer3/src/primer3_core*" "*psipred/src/seq2mtx.cpp" "*psipred/src/ssdefs*" "*ptools/src/ptools*" "*umuscle/src/muscle*" "*ugenem*" --output-file coverage_filtred.info
 "${LCOV_DIR}"genhtml coverage_filtred.info --output-directory coverage_report
 
