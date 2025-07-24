769529f769ba3927102d3e5686e37944f6deef1d
Dmitrii Sukhomlinov <dsukhomlinov@unipro.ru>
2025-07-24 09:30:54 +0700

    Fix script and add cleanup

--- etc/script/linux/run_coverage.sh
+++ etc/script/linux/run_coverage.sh
@@ -3,6 +3,11 @@
 TEAMCITY_WORK_DIR=$(pwd)
 SCRIPTS_DIR="${TEAMCITY_WORK_DIR}/ugene/etc/script/linux"
 
+echo "##teamcity[blockOpened name='Cleanup']"
+rm -r coverage_report
+rm coverage.info
+echo "##teamcity[blockClosed name='Cleanup']"
+
 echo "##teamcity[blockOpened name='Build UGENE']"
 #"${SCRIPTS_DIR}"/build.sh
 echo "##teamcity[blockClosed name='Build UGENE']"
@@ -13,8 +18,11 @@ echo "##teamcity[blockClosed name='Run XML Tests']"
 
 echo "##teamcity[blockOpened name='Run Coverage']"
 
-"${LCOV_DIR}"lcov --capture --directory ./ugene/cmake-build-relwithcov--output-file coverage.info --ignore-errors negative
-"${LCOV_DIR}"lcov --remove coverage.info "/usr/*" "/home/ugene/Qt/*" "*_autogen*" "*qrc_*" "*breakpad*" "*qscore/src/qscore*" "*QSpec*" "*samtools/src/samtools_core*" "*sqlite*" "*qtscript*" "*zlib*" "*plugins/api_tests*" "*plugins/GUITestBase*" "*ball/src/include*" "*ball/src/source*" "*gor4/src/gor.*" "*gor4/src/nrutil.*" "*hmm2/src/hmmer2*" "*phylip/src/neighbor.h" "*phylip/src/phylip.*" "*phylip/src/protdist.*" "*phylip/src/seq.*" "*phylip/src/seqboot.*" "*primer3/src/primer3_core*" "*psipred/src/seq2mtx.cpp" "*psipred/src/ssdefs*" "*ptools/src/ptools*" "*umuscle/src/muscle*" "*ugenem*" --output-file coverage_filtred.info
-"${LCOV_DIR}"genhtml coverage_filtred.info --output-directory coverage_report
+echo "Generate coverage"
+
+"${LCOV_DIR}"lcov --capture --directory ./ugene/cmake-build-relwithcov --exclude "/usr/*" --exclude "/home/ugene/Qt/*" --exclude "*_autogen*" --exclude "*qrc_*" --exclude "*breakpad*" --exclude "*qscore/src/qscore*" --exclude "*QSpec*" --exclude "*samtools/src/samtools_core*" --exclude "*sqlite*" --exclude "*qtscript*" --exclude "*zlib*" --exclude "*plugins/api_tests*" --exclude "*plugins/GUITestBase*" --exclude "*ball/src/include*" --exclude "*ball/src/source*" --exclude "*gor4/src/gor.*" --exclude "*gor4/src/nrutil.*" --exclude "*hmm2/src/hmmer2*" --exclude "*phylip/src/neighbor.h" --exclude "*phylip/src/phylip.*" --exclude "*phylip/src/protdist.*" --exclude "*phylip/src/seq.*" --exclude "*phylip/src/seqboot.*" --exclude "*primer3/src/primer3_core*" --exclude "*psipred/src/seq2mtx.cpp" --exclude "*psipred/src/ssdefs*" --exclude "*ptools/src/ptools*" --exclude "*umuscle/src/muscle*" --exclude "*ugenem*" --output-file coverage.info --ignore-errors negative,unused
+
+echo "Generate report"
+"${LCOV_DIR}"genhtml coverage.info --output-directory coverage_report --ignore-errors inconsistent
 
 echo "##teamcity[blockClosed name='Run Coverage']"
