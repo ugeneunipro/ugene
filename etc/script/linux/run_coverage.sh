332172f80e3709e591cdc825cbd484645bad539b
Dmitrii Sukhomlinov <dsukhomlinov@unipro.ru>
2025-07-24 09:04:14 +0700

    Repace Exclude with ignore errors

--- etc/script/linux/run_coverage.sh
+++ etc/script/linux/run_coverage.sh
@@ -13,7 +13,7 @@ echo "##teamcity[blockClosed name='Run XML Tests']"
 
 echo "##teamcity[blockOpened name='Run Coverage']"
 
-"${LCOV_DIR}"lcov --capture --directory ./ugene/cmake-build-relwithcov --exclude "*sqlite3.c" --output-file coverage.info
+"${LCOV_DIR}"lcov --capture --directory ./ugene/cmake-build-relwithcov--output-file coverage.info --ignore-errors negative
 "${LCOV_DIR}"lcov --remove coverage.info "/usr/*" "/home/ugene/Qt/*" "*_autogen*" "*qrc_*" "*breakpad*" "*qscore/src/qscore*" "*QSpec*" "*samtools/src/samtools_core*" "*sqlite*" "*qtscript*" "*zlib*" "*plugins/api_tests*" "*plugins/GUITestBase*" "*ball/src/include*" "*ball/src/source*" "*gor4/src/gor.*" "*gor4/src/nrutil.*" "*hmm2/src/hmmer2*" "*phylip/src/neighbor.h" "*phylip/src/phylip.*" "*phylip/src/protdist.*" "*phylip/src/seq.*" "*phylip/src/seqboot.*" "*primer3/src/primer3_core*" "*psipred/src/seq2mtx.cpp" "*psipred/src/ssdefs*" "*ptools/src/ptools*" "*umuscle/src/muscle*" "*ugenem*" --output-file coverage_filtred.info
 "${LCOV_DIR}"genhtml coverage_filtred.info --output-directory coverage_report
 
