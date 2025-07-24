cac9e58b59f3704ec3fa6c609d86b252329675eb
Dmitrii Sukhomlinov <dsukhomlinov@unipro.ru>
2025-07-24 15:06:12 +0700

    Enable XML Tests

--- etc/script/linux/run_coverage.sh
+++ etc/script/linux/run_coverage.sh
@@ -13,7 +13,7 @@ echo "##teamcity[blockOpened name='Build UGENE']"
 echo "##teamcity[blockClosed name='Build UGENE']"
 
 echo "##teamcity[blockOpened name='Run XML Tests']"
-#"${SCRIPTS_DIR}"/run_xml_tests.sh
+"${SCRIPTS_DIR}"/run_xml_tests.sh
 echo "##teamcity[blockClosed name='Run XML Tests']"
 
 echo "##teamcity[blockOpened name='Run GUI Tests']"
