2ccb1fad88614718cf8505f4cea826a7d113d736
Dmitrii Sukhomlinov <sukhomlinovdmitrii@yandex.ru>
2025-09-26 11:39:41 +0700

    1784 Error messages when running cmd-line version of UGENE on MacOS (#1785)

--- src/corelibs/U2Designer/src/PrompterBase.h
+++ src/corelibs/U2Designer/src/PrompterBase.h
@@ -39,7 +39,10 @@ class U2DESIGNER_EXPORT PrompterBaseImpl : public ActorDocument, public Prompter
     Q_OBJECT
 public:
     PrompterBaseImpl(Actor* p = 0) : ActorDocument(p) {
-        connect(AppContext::getMainWindow(), &MainWindow::si_colorThemeSwitched, this, &PrompterBaseImpl::sl_actorModified);
+        auto mw = AppContext::getMainWindow();
+        if (mw != nullptr) {
+            connect(mw, &MainWindow::si_colorThemeSwitched, this, &PrompterBaseImpl::sl_actorModified);
+        }
     }
 
     static bool isWildcardURL(const QString& url) {
