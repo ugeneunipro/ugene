0e1dd222e9c9c71a740313a0ee93a2e055ce3469
Dmitrii Sukhomlinov <sukhomlinovdmitrii@yandex.ru>
2026-01-27 11:39:48 +0700

    1870 MAFFT crash (#1901)

--- src/plugins/external_tool_support/src/mafft/MafftAddToAlignmentTask.cpp
+++ src/plugins/external_tool_support/src/mafft/MafftAddToAlignmentTask.cpp
@@ -224,11 +224,12 @@ void MafftAddToAlignmentTask::run() {
         }
         stateInfo.setProgress(70 + 30 * posInMsa / objectsCount);
         auto sequenceObject = qobject_cast<U2SequenceObject*>(object);
+        SAFE_POINT_NN(sequenceObject, );
+
         bool rowWasAdded = true;
         if (!rowNames.contains(sequenceObject->getSequenceName())) {
             // inserting new rows
             sequenceObject->setGObjectName(uniqueIdsToNames[sequenceObject->getGObjectName()]);
-            SAFE_POINT(sequenceObject != nullptr, "U2SequenceObject is null", );
 
             U2MsaRow row = MsaUtils::copyRowFromSequence(sequenceObject, settings.msaRef.dbiRef, stateInfo);
 
