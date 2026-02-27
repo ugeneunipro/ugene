0dd9c85666057c4ff79f4c4c38120b98c8d9d9af
Kirill Rasputin <rasputinkirill@gmail.com>
2026-02-27 11:16:17 +0700

    review issues

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -1,8 +1,8 @@
 # UGENE version definition
 
 set(UGENE_VER_MAJOR 53)
-set(UGENE_VER_MINOR 1)
-set(UGENE_VER_SUFFIX "") # optional, like "-dev" or ""
+set(UGENE_VER_MINOR 0)
+set(UGENE_VER_SUFFIX "-dev") # optional, like "-dev" or ""
 
 set(UGENE_VERSION "${UGENE_VER_MAJOR}.${UGENE_VER_MINOR}${UGENE_VER_SUFFIX}")
 
